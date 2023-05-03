e562b21a9de505ff1138ec56c5d62461427fa193
lowdennadine <lowdennadine@gmail.com>
2023-05-03 12:12:27 +0200

    May 3

---
+++ Assets/Scripts/GridObjectCollection.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class GridObjectCollection: MonoBehaviour
+   
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}*/
+
+
+
+[UnityEngine.AddComponentMenu("Scripts/MRTK/SDK/GridObjectCollection")]
+[UnityEngine.ExecuteAlways]
+[UnityEngine.HelpURL("https://learn.microsoft.com/windows/mixed-reality/mrtk-unity/features/ux-building-blocks/object-collection")]
+public class GridObjectCollection : Microsoft.MixedReality.Toolkit.Utilities.BaseObjectCollection
+{ }
+    public abstract class BaseObjectCollection : UnityEngine.MonoBehaviour
+{ }
